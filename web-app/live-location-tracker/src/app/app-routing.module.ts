bb35d68791a7a9681b9e2e8212cb4dc58bdee31a
Your Name <you@example.com>
2018-06-04 16:35:48 +0530

    #19 fixed login and logout feature using guards

--- web-app/live-location-tracker/src/app/app-routing.module.ts
+++ web-app/live-location-tracker/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'register' , component: RegisterComponent },
   { path: 'login' , component: LoginComponent},
-  { path: 'dashboard', component: DashboardComponent}
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: '**', component: PagenotfoundComponent }
 ];
 
 @NgModule({
--- web-app/live-location-tracker/src/app/header/header.component.ts
+++ web-app/live-location-tracker/src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '.././services/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: AuthService) { }
 
   ngOnInit() {
   }
